ad07b66ccbf046401a934e9987a783072fc66d71
Geoff Storbeck <storbeck@gmail.com>
2020-12-06 11:38:07 -0500

    day 1 complete

---
+++ day1.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const total = 2020;
+
+fs.readFile("day1.txt", "utf8", (err, data) => {
+  if (err) throw err;
+
+  const uniques = [...new Set(data.split("\n"))];
+
+  uniques.every((num) => {
+    const missing = "" + Math.abs(num - total);
+    if (uniques.includes(missing)) {
+      console.log(num * missing);
+      return false
+    }
+    return true
+  });
+});
