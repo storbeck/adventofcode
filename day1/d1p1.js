3e2f6c04144aa93bfdcee3572cbcac381e578db7
Geoff Storbeck <storbeck@gmail.com>
2020-12-07 11:15:44 -0500

    increase performance

--- day1/d1p1.js
+++ day1/d1p1.js
@@ -4,14 +4,13 @@ const total = 2020;
 fs.readFile("d1p1.txt", "utf8", (err, data) => {
   if (err) throw err;
 
-  const uniques = [...new Set(data.split("\n"))];
+  const uniques = new Set(data.split("\n"));
 
-  uniques.every((num) => {
+  for (let num of uniques) {
     const missing = "" + Math.abs(num - total);
-    if (uniques.includes(missing)) {
+    if (uniques.has(missing)) {
       console.log(num * missing);
-      return false
+      break
     }
-    return true
-  });
+  }
 });
